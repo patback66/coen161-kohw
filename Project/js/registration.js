3e2cd40463ab44cd8dc03b1135ae1dacc3716876
maskoken <maskoken@gmail.com>
2017-03-12 19:23:58 -0400

    updates for registration

--- Project/js/registration.js
+++ Project/js/registration.js
@@ -26,11 +26,89 @@ function submit(form) {
             console.log("[INFO] Successfully registered." );
             var response = JSON.parse(JSON.stringify(msg));
             console.log("[INFO] response.msg => " + response.msg);
-
+            return true;
         },
         error: function() {
             console.log("[ERROR] Could not register.")
+            return false;
         }
     });
-  return false;
+  //return false;
+}
+
+var counter = 1;
+var limit = 4;
+function addInput(divName){
+     if (counter == limit)  {
+          alert("You have reached the limit of adding " + counter + " inputs");
+     }
+     else {
+          counter++;
+          var newdiv = document.createElement('div');
+          newdiv.innerHTML = '<br><br> \
+                <div id="camper'+ counter + '"> \
+                <legend style="font-size: 20px;">Camper ' + counter + ' Information</legend> \
+                <div class="form-group"> \
+                  <label class="col-md-4 control-label">Camper First Name</label>  \
+                  <div class="col-md-4 inputGroupContainer"> \
+                  <div class="input-group"> \
+                  <span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span> \
+                  <input  name="parent_first_name" placeholder="First Name" class="form-control"  type="text"> \
+                    </div> \
+                  </div> \
+                </div>  \
+                    \
+                <!-- Text input--> \
+                    \
+                <div class="form-group"> \
+                  <label class="col-md-4 control-label" > Camper Last Name</label> \
+                    <div class="col-md-4 inputGroupContainer"> \
+                    <div class="input-group"> \
+                  <span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span> \
+                  <input name="parent_last_name" placeholder="Last Name" class="form-control"  type="text"> \
+                    </div> \
+                  </div> \
+                </div> \
+                          \
+                <!-- Text input--> \
+                       <div class="form-group"> \
+                  <label class="col-md-4 control-label">Camper E-Mail</label>  \
+                    <div class="col-md-4 inputGroupContainer"> \
+                    <div class="input-group"> \
+                        <span class="input-group-addon"><i class="glyphicon glyphicon-envelope"></i></span>\
+                  <input name="email" placeholder="E-Mail Address" class="form-control"  type="text"> \
+                    </div> \
+                  </div> \
+                </div> \
+                \
+                \
+                <!-- Text input--> \
+                \
+                <div class="form-group"> \
+                  <label class="col-md-4 control-label">Camper Cell #</label>  \
+                    <div class="col-md-4 inputGroupContainer"> \
+                    <div class="input-group"> \
+                        <span class="input-group-addon"><i class="glyphicon glyphicon-earphone"></i></span> \
+                  <input name="phone" placeholder="(845)555-1212" class="form-control" type="text"> \
+                    </div> \
+                  </div> \
+                </div> \
+                </div>';
+
+          document.getElementById(divName).appendChild(newdiv);
+          calcCost();
+     }
+}
+
+function calcCost() {
+  //get cost from radio button
+  var cost = parseInt($("input[name=hosting]:checked").val());
+  
+  //for each extra camper, discount
+  if(counter > 1) {
+    cost += (counter-1) * (cost * .9);
+  }
+  cost += 500;
+  document.getElementById("priceSpan").textContent = "$" + cost + " (Total includes $500 deposit.)";
+  document.getElementById("cost").value = cost;
 }
--- Project/php/charts.php
+++ Project/php/charts.php
@@ -28,7 +28,7 @@ function getCampersPerLocation() {
    $host="localhost"; // Host name 
     $username="client"; // Mysql username 
     $password="coen161"; // Mysql password 
-    $db_name="KOHW"; // Database name 
+    $db_name="KOHW"; // Database name   
     $con = mysqli_connect("$host", "$username", "$password") or die("Connection Problem" . mysqli_errno($con));
     $database = mysqli_select_db($con, "$db_name") or die("SQL Problem" . mysqli_error($con));
   
